8e34971b5071a83c31067f53a52b7a33ce194035
Delbighily <d.elbighily@gmail.com>
2024-04-16 22:09:23 +0200

    Final signup POM

--- cypress/PageObjectModel/SignUp.js
+++ cypress/PageObjectModel/SignUp.js
@@ -1,26 +1,34 @@
 class Signup
 {
+    //Locators 
+    fnameField="#firstname"
+    lnameField="#lastname"
+    emailField="#email_address"
+    passwordField="#password"
+    passwordConfiField="#password-confirmation"
+    submitBTN=".submit"
+
     checkPageTitle()
     {
         cy.title().should('eql','Create New Customer Account')
     }
     setUserName(fName,lName)
     {
-        cy.get('#firstname').should('be.visible').type(fName);
-        cy.get('#lastname').should('be.visible').type(lName);
+        cy.get(this.fnameField).should('be.visible').type(fName);
+        cy.get(this.lnameField).should('be.visible').type(lName);
     }
     setEmailPasswprd(mail,pass)
     {
-        cy.get('#email_address').should('be.visible').type(mail);
-        cy.get('#password').should('be.visible').type(pass);
+        cy.get(this.emailField).should('be.visible').type(mail);
+        cy.get(this.passwordField).should('be.visible').type(pass);
     }
     confirmPassword(pass)
     {
-        cy.get('#password-confirmation').should('be.visible').type(pass);
+        cy.get(this.passwordConfiField).should('be.visible').type(pass);
     }
     clickSubmit()
     {
-        cy.get('.submit').click();
+        cy.get(this.submitBTN).click();
     }
     AssertSuccess()
     {
