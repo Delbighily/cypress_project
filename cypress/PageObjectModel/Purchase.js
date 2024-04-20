4c8311adfbe2ff091001488469403f31d85a552f
Delbighily <d.elbighily@gmail.com>
2024-04-20 17:16:06 +0200

    Modifying POM for purchase page

--- cypress/PageObjectModel/Purchase.js
+++ cypress/PageObjectModel/Purchase.js
@@ -14,69 +14,119 @@ class Purchase
     actionBTN=".action-accept"
     cartCheck=".block-minicart.block"
 
+    //Elements Getters
+    getUpperTab()
+    {
+        return cy.get(this.upperTab);
+    }
+    getCategories()
+    {
+        return cy.get(this.categories);
+    }
+    getItemSelection()
+    {
+        return cy.get(this.itemSelection);
+    }
+    getSizeSelector()
+    {
+        return cy.get(this.sizeSelector);
+    }
+    getColorSelector()
+    {
+        return cy.get(this.colorSelector);
+    }
+    getQuantityField()
+    {
+        return cy.get(this.quantityField);
+    }
+    getSelectedLBL()
+    {
+        return cy.get(this.selectedLBL);
+    }
+    getAddToCartBTN()
+    {
+        return cy.get(this.addToCartBTN);
+    }
+    getCartBTN()
+    {
+        return cy.get(this.cartBTN);
+    }
+    getDeleteItemBTN()
+    {
+        return cy.get(this.deleteItemBTN);
+    }
+    getActionBTN()
+    {
+        return cy.get(this.actionBTN);
+    }
+    getCartCheck()
+    {
+        return cy.get(this.cartCheck);
+    }
 
+    //Interactions
     clickTab()
     {
-        cy.get(this.upperTab).click();
+        this.getUpperTab().click();
     }
 
     selectCategory()
     {
-        cy.get(this.categories).click();
+        this.getCategories().click();
     }
 
     selectItem()
     {
-        cy.get(this.itemSelection).click();
+        this.getItemSelection().click();
     }
 
     selectSize()
     {
-        cy.get(this.sizeSelector).wait(1000).click();
+        this.getSizeSelector().wait(1000).click();
     }
 
     selectColor()
     {
-        cy.get(this.colorSelector).wait(1000).click();
+        this.getColorSelector().wait(1000).click();
     }
 
     setQuantity()
     {
-        cy.get(this.quantityField).wait(1000).clear().type('3');
+        this.getQuantityField().wait(1000).clear().type('3');
     }
 
     assertSelectedSize()
     {
-        cy.get(this.selectedLBL).first().should('contain','M');
+        this.getSelectedLBL().first().should('contain','M');
     }
 
     assertSelectedColor()
     {
-        cy.get(this.selectedLBL).last().should('contain','Green');
+        this.getSelectedLBL().last().should('contain','Green');
     }
 
     clickAddToCart()
     {
-        cy.get(this.addToCartBTN).wait(500).click();
+        this.getAddToCartBTN().wait(500).click();
     }
 
     clickOnCart()
     {
-        cy.get(this.cartBTN).click();
+        this.getCartBTN().click();
     }
 
     deleteItems()
     {
-        cy.get(this.deleteItemBTN).click();
+        this.getDeleteItemBTN().click();
     }
 
     confirmDeletion()
     {
-        cy.get(this.actionBTN).click();
+        this.getActionBTN().click();
     }
 
     assertCorrectDeletion()
     {
-        cy.get(this.cartCheck).should('contain','You have no items in your shopping cart.')
+        this.getCartCheck().should('contain','You have no items in your shopping cart.')
     }
-}export default Purchase
+}export default Purchase;
